cb269363eeff72af2e54b776b0bf21a197af420c
axelsid <100712659+axelsid@users.noreply.github.com>
2026-05-12 20:05:29 +0200

    First draft of main code
    This is first draft of the final code

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from phoneusagedetection!")
+from Anonimizer import anonymize
+from phoneDetection import detect_phones
+import cv2
+
+file_path = "man-sitting-in-his-car-texting-with-his-cell-phone-side-view-of-a-young-man-sitting-inside-car-using-mobile-phone-young-man-sitting-in-a-car-and-looking-at-a-mobile-phone-photo.jpg"
+score_threshold = 0.6
+
+anonimized_image = anonymize(file_path, score_threshold)
+cv2.imwrite("image_anonimized.jpg", anonimized_image)
+isPhonesDetected = detect_phones("image_anonimized.jpg")
+if isPhonesDetected:
+    print("Phones detected!!!!!")
+else:
+    print("No phones detected!!!")
 
 
-if __name__ == "__main__":
-    main()
